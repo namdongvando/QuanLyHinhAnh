896caa5d32f3ce84e16023f19d95002932deef22
namdongvando <namdong92@gmail.com>
2021-06-16 22:07:49 +0700

    TaoMoudle

---
+++ Module/QuanLyHinhAnh/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Module\QuanLyHinhAnh;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of auth
+ *
+ * @author MSI
+ */
+class auth {
+
+    //put your code here
+    public function __construct() {
+
+    }
+
+}
